44588415829ec2dd1d411d80250426cd931df7d7
myschool9 <107306026+myschool9@users.noreply.github.com>
2022-07-06 16:37:14 +0300

    Add files via upload

--- rozklad/myscript.js
+++ rozklad/myscript.js
@@ -1,11 +1,15 @@
-function Filtering(nItem) {
+function Filtering(nItem, myTime, isTiming) {
+		const myStyle = 'width: 96%; max-width: 500px; margin-top: 45px; margin-left: 6px; border-bottom: dotted 1px #808080; display: table;';
     let mySel = document.getElementById('id_itemSelect');
-    if (nItem < 0 || nItem >= mySel.length) {
+		if (nItem < 0) {
         nItem = 0;
     }
+		if (nItem >= mySel.length) {
+				nItem = mySel.length - 1;
+		}
     for (let i = 0; i < mySel.length; i++) {
         if (i == nItem) {
-            document.getElementById('m' + String(nItem)).style.display = "table";
+            document.getElementById('m' + String(nItem)).style = myStyle;
         } else {
             document.getElementById('m' + String(i)).style.display = "none";
         }
@@ -13,27 +17,25 @@ function Filtering(nItem) {
     mySel.selectedIndex = nItem;
 
     /* замалювання поточного рядка */    
-    let n = GetCurrentNum();
-    let x = Number(location.href[location.href.indexOf('.html') - 1]);
+    let n = GetCurrentNum(myTime);
     document.getElementById('id_header').style.backgroundColor = "#8fbc8f";
-    if (x <= 2 && n == nItem) {
+    if (!isTiming && n == nItem) {
         document.getElementById('id_header').style.backgroundColor = "#f0e68c";
     }
-    if (x > 2) {
+    if (isTiming) {
         document.getElementById('m' + String(nItem) + '_'+ String(n)).style.backgroundColor = "#f0e68c";
     }
 }
 
-function GetCurrentNum() { /* визначення номера рядка, потрібно лише для замалювання поточного рядка! */
+function GetCurrentNum(myTime) { /* визначення номера рядка, потрібно лише для замалювання поточного рядка! */
     let d = new Date();
     let m = new Date();
     let g = new Date();
-    let time = [585, 645, 705, 780, 840, 900, 960];
     let day = d.getUTCDay();
     let min = 60 * g.getHours() + m.getMinutes();
 
     let i = 0;
-    while (i < 7 && time[i] + 3 <= min) {
+    while (i < 7 && myTime[i] + 3 <= min) {
         i++;
     }
     if (i==7) {
@@ -46,20 +48,4 @@ function GetCurrentNum() { /* визначення номера рядка, по
         day--;
     }
     return 7 * day + i;
-}
-
-function ActivateDoc() { /* завантаження одразу з фільтром */
-    Filtering(Number(location.search.slice(1)))
-}
-
-function ChangeWeek() { /* зміна чисельників на знаменники і навпаки */
-    let n = location.href.indexOf('.html') - 1;
-    let x = Number(location.href[n]);
-    if (x % 2 == 0) {
-        x--;
-    } else {
-        x++;
-    }
-    location.href = location.href.slice(0, n) + String(x) + '.html?' + 
-        String(document.getElementById('id_itemSelect').selectedIndex);
 }
