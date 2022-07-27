3049136382283c1c4a676019337ead8466080014
myschool9 <107306026+myschool9@users.noreply.github.com>
2022-07-27 19:06:06 +0300

    Add files via upload

--- css/timetable.js
+++ css/timetable.js
@@ -1,50 +1,50 @@
 function Filtering(nItem, myTime, isTiming) {
-    let mySel = document.getElementById('select');
-		if (nItem < 0) {
-			nItem = 0;
-		}
-		if (nItem >= mySel.length) {
-			nItem = mySel.length - 1;
-		}
-    for (let i = 0; i < mySel.length; i++) {
-        if (i == nItem) {
-            document.getElementById('m' + String(i)).style.display = "table";
-        } else {
-            document.getElementById('m' + String(i)).style.display = "none";
-        }
+  let mySel = document.getElementById('select');
+  if (nItem < 0) {
+    nItem = 0;
+  }
+  if (nItem >= mySel.length) {
+    nItem = mySel.length - 1;
+  }
+  for (let i = 0; i < mySel.length; i++) {
+    if (i == nItem) {
+      document.getElementById('m' + String(i)).style.display = "table";
+    } else {
+      document.getElementById('m' + String(i)).style.display = "none";
     }
-    mySel.selectedIndex = nItem;
+  }
+  mySel.selectedIndex = nItem;
 
     /* замалювання поточного рядка або шапки, якщо день - поточний */    
-    let n = GetCurrentNum(myTime);
-    document.getElementById('header').style.backgroundColor = "var(--header-color)";
-    if (!isTiming && n == nItem) {
-        document.getElementById('header').style.backgroundColor = "var(--light-color)";
-    }
-    if (isTiming) {
-        document.getElementById('m' + String(nItem) + '_'+ String(n)).style.backgroundColor = "var(--light-color)";
-    }
+  let n = GetCurrentNum(myTime);
+  document.getElementById('header').style.backgroundColor = "var(--header-color)";
+  if (!isTiming && n == nItem) {
+    document.getElementById('header').style.backgroundColor = "var(--light-color)";
+  }
+  if (isTiming) {
+    document.getElementById('m' + String(nItem) + '_'+ String(n)).style.backgroundColor = "var(--light-color)";
+  }
 }
 
 function GetCurrentNum(myTime) { /* визначення номера рядка, потрібно лише для замалювання поточного рядка! */
-    let d = new Date();
-    let m = new Date();
-    let g = new Date();
-    let day = d.getUTCDay();
-    let min = 60 * g.getHours() + m.getMinutes();
+  let d = new Date();
+  let m = new Date();
+  let g = new Date();
+  let day = d.getUTCDay();
+  let min = 60 * g.getHours() + m.getMinutes();
 
-    let i = 0;
-    while (i < 7 && myTime[i] + 3 <= min) {
-        i++;
-    }
-    if (i==7) {
-        i=6;
-    }
-    if (day==0 || day==6) {
-        day=0;
-        i=0;
-    } else {
-        day--;
-    }
-    return 7 * day + i;
+  let i = 0;
+  while (i < 7 && myTime[i] + 3 <= min) {
+    i++;
+  }
+  if (i==7) {
+    i=6;
+  }
+  if (day==0 || day==6) {
+    day=0;
+    i=0;
+  } else {
+    day--;
+  }
+  return 7 * day + i;
 }
