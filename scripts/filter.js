c214ee10542db2e711497562bd80f87b1a6e2d9d
myschool9 <107306026+myschool9@users.noreply.github.com>
2023-02-01 21:04:05 +0200

    Add files via upload

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,19 +1,19 @@
-function Filtering(nItem, isTotal) {
-	let mySel = document.getElementById('select');
+function Filtering(nItem, isFullList) {
+	let items = document.getElementById('listBox');
 	if (nItem < 0) {
 		nItem = 0;
 	}
-	if (nItem >= mySel.length) {
-		nItem = mySel.length - 1;
+	if (nItem >= items.length) {
+		nItem = items.length - 1;
 	}
 
-	for (let i = 1; i < mySel.length; i += 1) {
-		if (i === nItem || isTotal && nItem === 0) {
+	for (let i = 1; i < items.length; i += 1) {
+		if (i === nItem || isFullList && nItem === 0) {
 			document.getElementById('m' + String(i)).style.visibility = 'visible';
 		} else {
 			document.getElementById('m' + String(i)).style.visibility = 'collapse';
 		}
 	}
 
-	mySel.selectedIndex = nItem;
+	items.selectedIndex = nItem;
 }
