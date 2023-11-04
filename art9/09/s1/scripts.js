933c9ae5efe982928e213d4b9586da90a3369229
myschool9 <107306026+myschool9@users.noreply.github.com>
2023-11-04 11:51:10 +0200

    Add files via upload

---
+++ art9/09/s1/scripts.js
@@ -0,0 +1,58 @@
+function f1() {
+	div1.style.color = 'red';
+}
+
+function f2() {
+	div2.style.color = 'ForestGreen';
+	div2.style.fontWeight = 'bold';
+	div2.style.border = '2px dotted';
+}
+
+function f3() {
+	div3.style.visibility = 'hidden';
+	alert('Помилка!!!');
+}
+
+function f4() {
+	div4.style.backgroundColor = 'DeepPink';
+	div4.style.borderRadius = '5px';
+}
+
+function f5() {
+	div0.innerHTML = 'Результат - прекрасний';
+	div1.style.display = 'none';
+	div2.style.display = 'none';
+	div3.style.display = 'none';
+	div4.style.display = 'none';
+}
+
+
+
+/*
+id.style.fontSize = '24px';					зміна стилю
+id.innerHTML = 'Натисніть';					зміна тексту
+input1.value = '454';						зміна значення input
+
+alert('Дякуємо за користуванням сайтом');	вікно з повідомленням
+prompt('Введіть пароль');					вікно введення
+confirm('Ви дійсно хочете вийти?');			вікно з підтвердженням
+
+1.  font-family: Arial, Verdana, sans-serif;		шрифти
+2.  font-size: 14px;             (120%)				розмір
+3.  font-weight: bold;           (normal)			насиченість  (потовщений)
+4.  font-style: italic;          (normal)			стиль  (похилий)
+5.  text-decoration: underline;  (none)				підкреслення
+6.  color: red;                  (#80fde0)			колір
+7.  background-color: yellow;    (#b6ff2a)			колір тла
+
+12. float: left;                 (none)  (right)	обтікання
+13. width: 50%;										ширина
+		13а. height: 300px;							висота
+
+14. border-radius: 15px;							радіус округлення меж
+15. border: 3px solid black;						межі (товщина стиль колір)
+		15a. border-top:     15б. border-bottom:    15в. border-left:    15г. border-right:
+
+16. display: flex;               (none) (block) (inline)	тип блоку
+20. visibility: hidden;          (visible)					видимість
+*/
