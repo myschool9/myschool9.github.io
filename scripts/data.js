4f67f16e9985fcbb19357c61f716b4f3394d1764
myschool9 <107306026+myschool9@users.noreply.github.com>
2025-06-07 12:40:35 +0300

    Add files via upload

---
+++ scripts/data.js
@@ -0,0 +1,26 @@
+function CreateArray(parametrs) {
+	const symb = '&';
+	const par = decodeURI(parametrs).split(symb);
+
+	if (par.length < 3) return;
+
+	let folder = par[0].slice(1, par[0].length);
+	folder = '/' + folder.replace('-', '/') + '/';
+
+	CreateImagesArray(folder, par[1], par[2]);
+}
+
+/*---------------------------------------*/
+function CreateImagesArray(folder, imgFrom, imgTo) {
+	imgFrom = Number(imgFrom);
+	imgTo = Number(imgTo);
+	if (isNaN(imgFrom) || isNaN(imgTo) || imgFrom > 99 || imgTo > 99 || imgFrom <= 0 || imgTo <= 0 || imgFrom > imgTo) return;
+
+	let txt = '<div class="data-content">';
+	for (let i = imgFrom; i <= imgTo; i++) {
+		txt += '<div class="image-container"><img src="' + folder + 'Слайд' + String(i) + '.PNG" alt=""></div>';
+	}
+	txt += '</div>';
+
+	document.body.innerHTML = txt;
+}
